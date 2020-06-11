91a073ed81d4e9f76383944728827f570455a2eb
ghazal-aroosa <65502534+ghazal-aroosa@users.noreply.github.com>
2020-06-11 00:13:55 -0700

    initial commit

---
+++ src/Room.js
@@ -0,0 +1,48 @@
+import React,{ useState } from 'react';
+
+
+function Room() {
+  let [isLit, setLit] =useState(false);
+  let[age , setAge]= useState(23);
+  
+
+
+  /*function updateLit(){
+    console.log("Button clicked");
+    setLit(!isLit);
+    
+  }*/
+
+  /*const increaseAge= ()=>{
+    console.log("IncreaseAge Button clicked");
+    setAge(++age);
+  }*/
+    //below  we use direct the functionality of updateLit fun. in button tag //
+    //below  we use direct the functionality of increase Age fun. in button tag //
+    
+   return (
+    <div >
+     
+      <strong>This Room is</strong> {isLit? "Lit": "dark"}
+      <br/>
+
+     <b>Age:</b> {age}
+      <br/>
+
+      <button onClick={ ()=> setLit(!isLit) } >Toggle light</button>
+      <br/>
+
+      <button onClick ={ ()=>{
+         console.log("IncreaseAge Arrow Function");
+         setAge(++age); 
+         } }  >  
+         
+         Increase Age  </button>
+         
+
+      
+    </div>
+  );
+}
+
+export default Room;
